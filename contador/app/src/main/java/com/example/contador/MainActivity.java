308d0bed21bc560b6d86ee10a1b90eb8c8c40de2
Gabriel Dziecinny Ferreira <gabidzferreira@gmail.com>
2026-05-31 02:45:39 -0300

    :sparkles: feature: contador de cliques simples

---
+++ contador/app/src/main/java/com/example/contador/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.contador;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button button = findViewById(R.id.clique);
+        TextView textView = findViewById(R.id.mostraCliques);
+
+        contador(button, textView);
+    }
+
+    public static void contador(Button bttn, TextView tvw) {
+        bttn.setOnClickListener( view -> {
+            i++;
+            tvw.setText(String.valueOf(i));
+            bttn.setText("Clique de novo!");
+        });
+
+
+    }
+}
