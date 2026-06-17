5e32722c0a3e9b8ed1620ac1de5ea144b51cd5f6
gabriel <gabriel.df27@aluno.ifsc.edu.br>
2026-06-16 21:40:23 -0300

    Test

---
+++ geolocalizacao/app/src/main/java/com/example/geolocalizacao/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.geolocalizacao;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+
+    @SuppressLint("ServiceCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.textView);
+
+        checkLocationPermission();
+
+        locationManager = (LocationManager) getSystemService(LOCALE_SERVICE);
+
+        locationManager.requestLocationUpdates(
+                locationManager.GPS_PROVIDER,
+                0,0,
+                location -> {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    tv.setText("Latitude: "+ latitude + "\nLongitude: " + longitude);
+                });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            } else {
+
+            }
+        }
+    }
+
+    public void checkLocationPermission() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+    }
+
+
+}
